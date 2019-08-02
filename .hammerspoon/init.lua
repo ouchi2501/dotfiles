ada4d210e61fd1b1810184496b6ed942a9cd98e4
uncle <uncle@fout.jp>
2019-08-02 11:55:04 +0900

    add hammerspoon conf

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,27 @@
+local function keyCode(key, modifiers)
+   modifiers = modifiers or {}
+   return function()
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+      hs.timer.usleep(1000)
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()      
+   end
+end
+
+local function keyCodeSet(keys)
+   return function()
+      for i, keyEvent in ipairs(keys) do
+         keyEvent()
+      end
+   end
+end
+
+local function remapKey(modifiers, key, keyCode)
+   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+end
+
+remapKey({ 'ctrl' }, '[',  keyCodeSet({
+    keyCode('escape'),
+    keyCode(';', {'ctrl', 'shift'})
+}))
+
+hs.alert.show("Config loaded")
